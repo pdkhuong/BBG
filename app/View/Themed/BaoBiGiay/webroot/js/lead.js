aae258b116db1b69383c7419db65c7d9339512a4
phuchongphuc <phanhongphuc@gmail.com>
2015-10-23 00:13:57 +0700

    Validate form Contact, reset Modal Contact

--- app/View/Themed/BaoBiGiay/webroot/js/lead.js
+++ app/View/Themed/BaoBiGiay/webroot/js/lead.js
@@ -10,9 +10,30 @@ $(document).ready(function() {
 	  var contactAddress = $("#_contact_address").val();
 	  var contactInfo = $("#_contact_info").val();
 	  var contactFax = $("#_contact_fax").val();
+      
+	  var reg_mail = /^[A-Za-z0-9]+([_\.\-]?[A-Za-z0-9])*@[A-Za-z0-9]+([\.\-]?[A-Za-z0-9]+)*(\.[A-Za-z]+)+$/;
+	  var checkEmail = true;
+	  if(!reg_mail.test(contactEmail) && contactEmail){
+		checkEmail = false;
+	  }
+	  var checkPhone = true;
+	  if((isNaN(contactPhone) || contactPhone.length<10 || contactPhone.length>11) && contactPhone){
+		checkPhone = false;
+	  }
+	  var checkFax = true;
+	  if((isNaN(contactFax) || contactFax.length<10 || contactFax.length>11) && contactFax){
+		checkFax = false;
+	  }
       if(!contactName){
         errorMsg = 'Please input name';
-      }
+      }else if(!checkEmail){
+		errorMsg = 'Please input a valid email address';
+	  }else if(!checkPhone){
+		errorMsg = 'Please input a valid phone number';
+	  }else if(!checkFax){
+		errorMsg = 'Please input a valid fax number';
+	  }
+	  
       if(errorMsg){
         $('#_contact_msg').removeClass("hidden");
         $('#_contact_msg').html(errorMsg);
@@ -45,13 +66,26 @@ $(document).ready(function() {
 		var selectedRow = $(this).parent().parent();
 		selectedRow.remove();
 	});
-	function resetModalContactData(){
-		$('#contact_id').val(null);
+	
+	function resetModalContactData(){		
+		$('#_contact_id').val(null);
+		$("#_contact_name").val(null);
+	    $("#_contact_email").val(null);
+	    $("#_contact_phone").val(null);
+	    $("#_contact_address").val(null);
+	    $("#_contact_info").val(null);
+	    $("#_contact_fax").val(null);		
+		$('#_contact_msg').addClass("hidden");
+        $('#_contact_msg').html("");
 		$( "._removeContact" ).click(function() {
 			var selectedRow = $(this).parent().parent();
 			selectedRow.remove();
 		});
 	}
+	
+	$('#contactModal').on('hidden.bs.modal', function () {
+		resetModalContactData();
+	})
 	
 	//edit contact
 	clickEdit();
